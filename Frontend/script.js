37c48287b657eedc269f00976d6f46501d5a95ae
Ritesh <kothawaderitesh2010@gmail.com>
2023-09-03 20:44:57 +0530

    frontend deploysed

--- Frontend/script.js
+++ Frontend/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const debugBtn = document.getElementById("debugBtn");
     const qualityBtn = document.getElementById("qualityBtn");
     const responseContainer = document.getElementById("responseContainer");
-    const baseURL = 'http://code-convertor-app.onrender.com/'
+    const baseURL = 'https://code-convertor-app.onrender.com/'
 
     codeForm.addEventListener("submit", async(e) => {
         e.preventDefault();
